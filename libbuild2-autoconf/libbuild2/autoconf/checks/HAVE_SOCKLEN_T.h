8523e46c91b6f7df654675bb94f0c6353307f61d
helmesjo <helmesjo@gmail.com>
2024-12-14 16:01:54 +0100

    Update HAVE_ASM_TYPES_H.h HAVE_GMTIME_R.h HAVE_LSTAT.h HAVE_SOCKLEN_T.h HAVE_STRUCT_GROUP_SOURCE_REQ.h HAVE_STRUCT_IP_MREQ_SOURCE.h socklen_t.h: Specifically for MacOS

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SOCKLEN_T.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SOCKLEN_T.h
@@ -1,7 +1,18 @@
-// HAVE_SOCKLEN_T: socklen_t
+// HAVE_SOCKLEN_T
 
 #undef HAVE_SOCKLEN_T
 
-#ifdef socklen_t
+/* socklen_t is available in:
+ * - glibc since 2.0
+ * - BSD systems (including MacOS)
+ * - Solaris, Cygwin
+ */
+#  if defined(__linux__)                  || \
+      defined(__FreeBSD__)                || \
+      defined(__OpenBSD__)                || \
+      defined(__NetBSD__)                 || \
+      defined(__APPLE__)                  || \
+      (defined(__sun) && defined(__SVR4)) || \
+      defined(__CYGWIN__)
 #  define HAVE_SOCKLEN_T 1
 #endif
